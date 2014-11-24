a4a47489b81d9eb16ba610ae7ae248f5459c7116
Richard Steen <richard.steen@activenetwork.com>
2014-11-24 10:41:55 -0600

    remove all completed todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,5 +1,5 @@
 Todos.TodosController = Ember.ArrayController.extend({
-  actions: {
+  actions: { 
     createTodo: function() {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
@@ -17,7 +17,12 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
-    }
+    },
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    } 
   },
 
   remaining: function() {
@@ -27,5 +32,13 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')  
+  }.property('remaining')  ,
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')    
 });
